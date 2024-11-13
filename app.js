a9332c3875d59b27b937595c804b69f35da1d27c
satyendra.foxaisr <satyendra.foxaisr@gmail.com>
2024-11-13 19:25:18 +0530

    fixes

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const Post = require('./model/user')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// API routes
 const data = require('./routes/index')
 app.use('/', data)
 
 
 // Create and save documents
-async function createData() {
-    const user = new User({ name: 'John Doe' });
+app.get('/post', async (req, res) => {
+    const user = new User({ name: 'saten' });
     await user.save();
 
     const post = new Post({
@@ -24,20 +25,39 @@ async function createData() {
         content: 'This is the content of my first post.',
         author: user._id
     });
+    res.send("call ")
     await post.save();
 
     console.log('Data created');
 }
+)
+
+
+// Or
+
+// Create and save documents
+// async function createData() {
+//     const user = new User({ name: 'John Doe' });
+//     await user.save();
+
+//     const post = new Post({
+//         title: 'My First Post',
+//         content: 'This is the content of my first post.',
+//         author: user._id
+//     });
+//     await post.save();
+
+//     console.log('Data created');
+// }
+
 
 // Querying with populated references
 
 app.get('/', async (req, res) => {
     const posts = await Post.find().populate('author');
     console.log(posts);
-
+    res.send("data fetched")
 })
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
--- model/user.js
+++ model/user.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name:String,
+    name: String,
 
 });
 const postSchema = new mongoose.Schema({
     title: String,
     content: String,
-    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 module.exports = mongoose.model('User', userSchema)
 module.exports = mongoose.model('Post', postSchema)
 
-// this is commit
+
